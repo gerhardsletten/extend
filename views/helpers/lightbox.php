2561b110a74fecc6203907967a8124da10a9146a
Gerhard Sletten <gerhardsletten@gmail.com>
2009-06-06 08:37:47 +0000

    update extend plugin
    git-svn-id: https://gerhardsletten.googlecode.com/svn/trunk/cakephp/plugins/extend@241 c0042060-deb1-11dd-87d8-31aa04531ed5

--- views/helpers/lightbox.php
+++ views/helpers/lightbox.php
@@ -4,7 +4,13 @@ class LightboxHelper extends AppHelper {
     var $helpers = array('Html', 'GershImage');
 
     function img($thumb = null, $full, $imgAttributes = array(), $linkAttributes = array(), $thumb_class = 'thumb', $full_class = 'full_lightbox') {
-        $defaultLinkAttributes = array('class'=>'thickbox');
+		$img_settings = Configure::read('Image.lightboxclass');
+		if(!empty($img_settings)) {
+			$defaultLinkAttributes = array('class'=>$img_settings);
+		} else {
+			$defaultLinkAttributes = array('class'=>'lightbox');
+		}
+        
         $linkAttributes = array_merge($defaultLinkAttributes, $linkAttributes);
 
 		// Gersh: remove 'img/' 
